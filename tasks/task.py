6c4cc210763af2c1494a67e5a56804b18ac55b50
martinkecher <martin.yaary@gmail.com>
2024-08-02 23:14:34 -0400

    Task class was created with its __init__ method

--- tasks/task.py
+++ tasks/task.py
@@ -1,1 +1,10 @@
 # This should ba an abstract class, there should be a few types of tasks: a timed task, shopping list and more that will inherit from Task.
+
+from datetime import datetime
+
+class Task:
+    def __init__(self, title: str, description: str = "", due_date: datetime = datetime.now) -> None:
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.created = datetime.now
